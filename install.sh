ff4ca61f6de9344739dccd213978c5b1f5c5c04d
trhacknon <98242014+trhacknon@users.noreply.github.com>
2022-09-11 15:09:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Author: github.com/dhasirar
-#Instagram: @dhasirar
+#Author: github.com/trhacknon
+#Instagram: @trhacknon
 trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 
 checkroot() {
